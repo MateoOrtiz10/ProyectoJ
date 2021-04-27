5658d46cd1094da442eed5c19bb0bb46d13f77d5
MateoOrtiz10 <l17420131@jiquilpan.tecnm.mx>
2021-04-26 23:47:02 -0500

    Aqui se realizo la creacion de la app

---
+++ javes-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">        
+        <p>
+          Hola, esto es parte de un repositorio
+        </p>              
+    </div>
+  );
+}
+
+export default App;
