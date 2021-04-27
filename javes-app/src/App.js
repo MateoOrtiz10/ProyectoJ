5ceef5d4f01671a01959857f5b5eb01b0147f481
MateoOrtiz10 <l17420131@jiquilpan.tecnm.mx>
2021-04-27 16:36:59 -0500

    Edicion de parrafo

--- javes-app/src/App.js
+++ javes-app/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">        
         <p>
-          Hola, esto es parte de un repositorio
+          Hola, esto es parte de un repositorio. Hola
         </p>              
     </div>
   );
