57babff12895c5d51a66b3333fab0aaa63b3af95
MateoOrtiz10 <l17420131@jiquilpan.tecnm.mx>
2021-04-28 15:07:57 -0500

    Limpieza de cosas

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ProyectoJ
 Proyecto de la empresa JAVES
 1. Se necesito instalar en la terminal npm install, para poder ejecutar el comando yarn start
-2. Se instalo bootstrap en el proyecto con el comando npm install react-bootstrap bootstrap
+2. Se instalo bootstrap en el proyecto con el comando: npm install react-bootstrap bootstrap
--- javes-app/src/App.jsx
+++ javes-app/src/App.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import { Route } from 'react-router';
 import Header from './Components/Header';
-import Menu from './Components/Menu';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className='jumbotron'>
-        <Header/>
-        <Route>
-        <Navbar />
-        <switch>
-          <Route path='/'/>
-          </switch>
-        </Route>
-        
+        <Header/>       
     </div>
   );
 }
