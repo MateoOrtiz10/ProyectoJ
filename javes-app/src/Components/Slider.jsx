d86e9c991e7d79fe2fa47bb65eb267d22dd4aca9
kelvin7799 <l17420141@jiquilpan.tecnm.mx>
2021-05-03 13:25:15 -0500

    imagenes

--- javes-app/src/Components/Slider.jsx
+++ javes-app/src/Components/Slider.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {Carousel} from 'react-bootstrap';
 import image1 from './Img/1.jpg';
 import image2 from './Img/2.jpg';
+import image3 from './Img/3.jpg';
+import image4 from './Img/Tas bien.jpg';
 import '../Style/javes.css';
 const CarouselContainer=() =>{
   return(
@@ -18,8 +20,26 @@ const CarouselContainer=() =>{
   </Carousel.Item>
   <Carousel.Item>
     <img
-      className="d-block w-100"
+      className="d-block w-100 ImgSlider"
       src={image2}
+      alt="Second slide"
+    />
+    <Carousel.Caption>    
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100 ImgSlider"
+      src={image3}
+      alt="Second slide"
+    />
+    <Carousel.Caption>    
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100 ImgSlider"
+      src={image4}
       alt="Second slide"
     />
     <Carousel.Caption>    
