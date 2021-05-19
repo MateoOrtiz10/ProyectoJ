50eb46a5e8b79f1e1b0a291f6223bcdd41908f8a
MateoOrtiz10 <l17420131@jiquilpan.tecnm.mx>
2021-05-18 22:54:05 -0500

    Borradero de cosas

--- javes-app/src/App.jsx
+++ javes-app/src/App.jsx
@@ -1,38 +1,13 @@
 import React from 'react';
-import './App.css'; 
 import Header from './Components/Header';
-import Navbar from './Components/Navbar';
-import * as ReactBootStrap from "react-bootstrap";
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Departamentos from './Pages/Depertamentos';
-import Dif from './Pages/Dif';
-import Oapas from './Pages/Oapas';
-import Obraspublicas from './Pages/Obraspublicas';
-import Sindicatura from './Pages/Sindicatura';
-import Tesoreria from './Pages/Tesoreria';
 import CarouselContainer from './Components/Slider';
-import Menudeabajo from'./Components/Menu_de_Abajo';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <>
-    <div className='jumbotron'>
-        <Header/>                     
-    </div>
-    <Router>
-        <Navbar/>
-        <Switch>
-          <Route path='/departamentos' exact component={Departamentos}/>
-          <Route path='/dif' component={Dif}/>
-          <Route path='/oapas' component={Oapas}/>
-          <Route path='/obraspublicas' component={Obraspublicas}/>
-          <Route path='/sindicatura' component={Sindicatura}/>
-          <Route path='/tesoreria' component={Tesoreria}/>
-        </Switch>
-      </Router>
-      <CarouselContainer/>
-      <Menudeabajo/>
-  </>
- 
+        <Header/>                            
+        {/* <CarouselContainer/>     */}
+ </>
   );
 }
 
--- javes-app/src/Components/Slider.jsx
+++ javes-app/src/Components/Slider.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import {Carousel} from 'react-bootstrap';
-import image1 from './Img/1.jpg';
-import image2 from './Img/2.jpg';
-import image3 from './Img/3.jpg';
-import image4 from './Img/Tas bien.jpg';
+import image1 from '../Img/1.jpg';
+import image2 from '../Img/2.jpg';
+import image3 from '../Img/3.jpg';
 import '../Style/javes.css';
 const CarouselContainer=() =>{
   return(
-      
-    <Carousel>
+  <Carousel>
   <Carousel.Item>
     <img 
       className="d-block w-100 ImgSlider" 
@@ -35,16 +33,7 @@ const CarouselContainer=() =>{
     />
     <Carousel.Caption>    
     </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100 ImgSlider"
-      src={image4}//Tas bien?
-      alt="Second slide"
-    />
-    <Carousel.Caption>    
-    </Carousel.Caption>
-  </Carousel.Item>
+  </Carousel.Item> 
 </Carousel>
   )  
 }
