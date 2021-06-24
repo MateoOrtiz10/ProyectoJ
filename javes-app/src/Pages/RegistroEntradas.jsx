ec1d9ff160878de9d7e8b9f0910bb652a56514a8
MateoOrtiz10 <l17420131@jiquilpan.tecnm.mx>
2021-06-24 00:43:37 -0500

    Conexion a la base de datos y pruebas

--- javes-app/src/Pages/RegistroEntradas.jsx
+++ javes-app/src/Pages/RegistroEntradas.jsx
@@ -2,25 +2,54 @@ import React from 'react';
 import '../Style/javes.css';
 import AppFrame from '../Components/AppFrame/AppFrame';
 import Grid from '@material-ui/core/Grid';
+import { useForm } from 'react-hook-form';
+import alertify from 'alertifyjs';
+import 'alertifyjs/build/css/alertify.min.css';
+import 'alertifyjs/build/css/themes/default.min.css';
+import {Link} from 'react-router-dom';
 
+const RegistroEnt = () => {
+    const URL_API='http://localhost/api-demo/insertar_registro_entrada.php';
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const onSubmit = data => {
+        // console.log(data);
+        fetch(URL_API, {
+            method:'POST',
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify(data)          
+        })
+        .then( resp=> resp.json () )
+        .then(json=>{
+            reset();
+            console.log("Respuesta:", json);
+            alertify.alert("Información de Envío", json.msj);
+        })
+    };
+    console.log(errors);
 
-const RegisteEmployed = () => {
-    return (
+    return (       
         <AppFrame>
-            <Grid container justify="center" direction="colum"> 
+            <Grid container justify="center"> 
             <h3 className="titulo">Registro de Entradas</h3> <br/>   
             <Grid item xs={12}>
-            <form className="formulario" >
+            <form className="formulario" onSubmit={handleSubmit(onSubmit)}>
                 <label>Nombre:</label><br />
-                <input type="text" className="form-control text-center" /><br />
+                <input type="text" className="form-control text-center" name="NombreTrab" {...register("NombreTrab", {required: true})} />
+                {errors.NombreTrab && <span className='text-danger'>Se requiere el nombre</span>}
+                <br />                
                 <label>Fecha:</label><br />
-                <input type="date" className="form-control text-center"></input><br />
-                <label>Hora de Entrada:</label><br />
-                <input type="time" className="form-control text-center"></input><br />
+                <input type="date" className="form-control text-center" name="Fecha" {...register("Fecha", {required: true})}></input>
+                {errors.Fecha && <span className="text-danger">Se requiere la fecha</span>}
+                <br />
+                <label>Hora de Entrada (24hrs):</label><br />
+                <input type="time" className="form-control text-center" name="HorarioEntrada" {...register("HorarioEntrada", {required: true})} ></input>
+                {errors.HorarioEntrada && <span className="text-danger">Se requiere el horario</span>}
+                <br />
                 <button type="submit" className="botonenes">Guardar</button>                
                 <button type="reset" className="botonenes">Borrar</button>
-            </form> <br />        
-            </Grid>                                        
+            </form> <br />                   
+            </Grid>
+            <Link to='main'>VOLVER</Link>                                         
             </Grid>            
         </AppFrame>         
         
@@ -28,4 +57,5 @@ const RegisteEmployed = () => {
     )
 }
 
-export default RegisteEmployed
+export default RegistroEnt;
+
--- javes-app/src/Pages/RegistroSalidas.jsx
+++ javes-app/src/Pages/RegistroSalidas.jsx
@@ -2,27 +2,57 @@ import React from 'react';
 import '../Style/javes.css';
 import AppFrame from '../Components/AppFrame/AppFrame';
 import Grid from '@material-ui/core/Grid';
+import { useForm } from 'react-hook-form';
+import alertify from 'alertifyjs';
+import 'alertifyjs/build/css/alertify.min.css';
+import 'alertifyjs/build/css/themes/default.min.css';
+import {Link} from 'react-router-dom';
 
-const RegisterOut = () => {
+const RegistroSal = () => {
+    const URL_API='http://localhost/api-demo/insertar_registro_salida.php';
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const onSubmit = data => {
+        // console.log(data);
+        fetch(URL_API, {
+            method:'POST',
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify(data)          
+        })
+        .then( resp=> resp.json () )
+        .then(json=>{
+            reset();
+            console.log("Respuesta:", json);
+            alertify.alert("Información de Envío", json.msj);
+        })
+    };
+    console.log(errors);
+
     return (
         <AppFrame>            
-            <Grid container justify="center" direction="colum">
+            <Grid container justify="center">
             <h3 className="titulo">Registro de Salidas</h3><br/>            
                 <Grid item xs={12}>
-                <form className="formulario">
+                <form className="formulario" onSubmit={handleSubmit(onSubmit)}>
                 <label>Nombre:</label><br/>
-                <input type="text" className="form-control text-center" /><br />
+                <input type="text" className="form-control text-center" name="NombreTrab" {...register("NombreTrab", {required: true})} />
+                {errors.NombreTrab && <span className='text-danger'>Se requiere el nombre</span>}
+                <br />                
                 <label>Fecha:</label><br/>
-                <input type="date" className="form-control text-center"></input><br />
-                <label>Hora de Salida:</label><br/>
-                <input type="time" className="form-control text-center"></input><br />
+                <input type="date" className="form-control text-center" name="Fecha" {...register("Fecha", {required: true})}></input>
+                {errors.Fecha && <span className="text-danger">Se requiere la fecha</span>}
+                <br />
+                <label>Hora de Salida (24hrs):</label><br/>
+                <input type="time" className="form-control text-center" name="HorarioSalida" {...register("HorarioSalida", {required: true})} ></input>
+                {errors.HorarioSalida && <span className="text-danger">Se requiere el horario</span>}
+                <br />
                 <button type="submit" className="botonenes">Guardar</button>
                 <button type="reset" className="botonenes">Borrar</button>
             </form> <br />  
-                </Grid>                
-            </Grid>                  
+                </Grid>
+                <Link to='main'>VOLVER</Link>                   
+                </Grid>                  
         </AppFrame>
     )
 }
 
-export default RegisterOut
+export default RegistroSal;
